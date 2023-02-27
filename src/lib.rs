3f9dc71a876bee103dceddde5d49405e7ea38763
Gobus <florian-c-hoffmann@t-online.de>
2023-02-27 17:01:35 +0100

    triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use wgpu::{
-    Backends, CommandEncoderDescriptor, Features, Limits, SurfaceConfiguration, TextureUsages,
-    TextureViewDescriptor,
+    include_wgsl, Backends, BlendState, ColorTargetState, ColorWrites, CommandEncoderDescriptor,
+    Features, FragmentState, Limits, MultisampleState, PipelineLayoutDescriptor, PrimitiveState,
+    RenderPipeline, RenderPipelineDescriptor, SurfaceConfiguration, TextureUsages,
+    TextureViewDescriptor, VertexState,
 };
 use winit::{
     event::*,
@@ -68,6 +70,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
+    render_pipeline: RenderPipeline,
 }
 impl State {
     // Creating some of the wgpu types requires async code
@@ -113,6 +116,46 @@ impl State {
             alpha_mode: surface_caps.alpha_modes[0],
             view_formats: vec![],
         };
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+        let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+        let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            primitive: PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            fragment: Some(FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(ColorTargetState {
+                    format: config.format,
+                    blend: Some(BlendState::REPLACE),
+                    write_mask: ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+        });
         Self {
             surface,
             device,
@@ -120,6 +163,7 @@ impl State {
             config,
             size,
             window,
+            render_pipeline: pipeline,
         }
     }
 
@@ -153,7 +197,7 @@ impl State {
                 label: Some("Render Encoder"),
             });
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -170,8 +214,9 @@ impl State {
                 })],
                 depth_stencil_attachment: None,
             });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
-
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
 
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -18,3 +18,7 @@ fn vs_main(
     
 }
 
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(0.9, 0.2, 0.1, 1.0);
+}
